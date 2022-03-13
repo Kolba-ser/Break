9744c67fad5009295a77150f6573b85367348c41
Vasiliy <hellemployee666@gmail.com>
2022-03-13 17:18:24 +0300

    [Added] Camera control

--- Assets/Scripts/Movement/SpiderMovement.cs
+++ Assets/Scripts/Movement/SpiderMovement.cs
@@ -7,28 +7,33 @@ namespace Break.Scripts.Movement
     {
         [SerializeField] private float speed;
 
-        private Controls controls;
+        [Header("Input settings")]
+        [SerializeField] private KeyCode forward;
+        [SerializeField] private KeyCode backward;
+        [SerializeField] private KeyCode right;
+        [SerializeField] private KeyCode left;
+
         private Rigidbody rigidbody;
         private RaycastHit hit;
+        private Vector3 direction;
 
         private void Awake()
         {
-            controls = new Controls();
-            controls.Enable();
             rigidbody = GetComponent<Rigidbody>();
+            direction = new Vector3();
         }
 
-        private void OnDestroy()
+        private void Update()
         {
-            controls.Dispose();
+            var dir1 = System.Convert.ToInt16(Input.GetKey(forward)) + (-System.Convert.ToInt16(Input.GetKey(backward)));
+            var dir2 = System.Convert.ToInt16(Input.GetKey(right)) + (-System.Convert.ToInt16(Input.GetKey(left)));
+
+            direction.x = dir2;
+            direction.z = dir1;
         }
 
         private void FixedUpdate()
         {
-            var direction = new Vector3(controls.Player.LeftRight.ReadValue<float>(),
-                0,
-                controls.Player.ForwardBackward.ReadValue<float>());
-
             var ray = new Ray(transform.position, CMath.Down);
             Physics.Raycast(ray, out hit);
             var d = direction - Vector3.Dot(direction, hit.normal) * hit.normal;
